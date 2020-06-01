53c0447afb0c9c028bccf3d1657c493ca46c3907
fruityvegetables <gg.troyer@gmail.com>
2020-06-01 18:33:22 -0500

    added some colors and switched the gif image and links in header/footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,24 +4,6 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div>
-              <p>
-                  <a href="https://github.com/fruityvegetables" target="blank">Github</a>
-              </p>
-              <p>
-                  <a href="https://www.pnkedin.com/in/gtroyer/" target="blank">LinkedIn</a>
-              </p>
-              <p>
-                  <a href="https://stackexchange.com/users/17871593/fruityvegetables" target="blank">StackExchange</a>
-              </p>
-              <p>
-                  <a href="https://www.facebook.com/FruityVegetables" target="blank">FaceBook</a>
-              </p>
-              <p>
-                  <a href="mailto:gg.troyer@gmail.com" target="blank">Email Me!</a>
-              </p>
-              
-      </div>
     </footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,24 @@ import "./Header.css";
 function Header() {
   return (<div>
     <header className="header">
+    <div>
+              <p>
+                  <a href="https://github.com/fruityvegetables" target="blank">Github</a>
+              </p>
+              <p>
+                  <a href="https://www.pnkedin.com/in/gtroyer/" target="blank">LinkedIn</a>
+              </p>
+              <p>
+                  <a href="https://stackexchange.com/users/17871593/fruityvegetables" target="blank">StackExchange</a>
+              </p>
+              <p>
+                  <a href="https://www.facebook.com/FruityVegetables" target="blank">FaceBook</a>
+              </p>
+              <p>
+                  <a href="mailto:gg.troyer@gmail.com" target="blank">Email Me!</a>
+              </p>
+              
+      </div>
     </header>
     </div>
   );
